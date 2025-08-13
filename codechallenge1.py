28a35b5233accda3eb8575e8ee22f0c61dafd3d3
Joseph <josefuu91@gmail.com>
2025-08-13 10:51:12 +0800

    Add files via upload

---
+++ codechallenge1.py
@@ -0,0 +1,3 @@
+name = input ("What is your name?")
+
+print ("\t\t\t\t\t*\n\t\t\t\t*\t\t*\t\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t Hi\t\t\t*\t\n\t*\t\t\t\t{}\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*".format(name))
