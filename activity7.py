cbc7503b0ebd0101580bf9dfcfbc8c20578e45d4
Joseph <josefuu91@gmail.com>
2025-08-18 11:16:44 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,23 @@
+j = 15
+
+print("The value of j is", j)
+
+j += 15
+
+print("The value of j is", j)
+
+j += 20
+
+print("The value of j is", j)
+
+j -= 35
+
+print("The value of j is", j)
+
+j *= 20
+
+print("The value of j is", j)
+
+j /= 5
+
+print("The value of j is", j)
---
+++ activity8.py
@@ -0,0 +1,15 @@
+balance = 3000
+withdrawal = 1500
+
+print(balance > withdrawal)
+ 
+j = 100
+
+p = 100
+
+print (j <= p)
+
+n1 = "Joseph Afable"
+n2 = "Joseph Afable"
+
+print (n1 > n2)
