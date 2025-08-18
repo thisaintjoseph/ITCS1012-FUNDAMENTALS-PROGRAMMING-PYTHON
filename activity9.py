cb2947bf65772d50d08d4688d55516afe3379510
Joseph <josefuu91@gmail.com>
2025-08-18 11:23:44 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,7 @@
+username = 'IamZeno' 
+password = '1121234'
+
+#print((username == 'IamZeno') and (password == '112123'))
+#print((username == 'IamZeno') or (password == '112123'))
+print(not((username == 'IamZeno') and (password == '112123')))
+
