413d7b7b5bd4ae1b63c5757dbca6d8bf06827fb1
Joseph <josefuu91@gmail.com>
2025-11-30 14:08:58 +0800

    Add files via upload

---
+++ codechallenge3.py
@@ -0,0 +1,13 @@
+
+
+username = 'thsiaintjosip'
+password = 'akonalangsana'
+
+name = input("Input your username ---> ")
+pass_word = input("Enter your password ---> ")
+
+if username == name and password == pass_word:
+	print("Access Granted")
+
+else:
+	print("Access Denied")
