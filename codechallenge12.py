fb605e084aab6d8054acfa79ff16685c2e6fb6d8
Joseph <josefuu91@gmail.com>
2025-11-30 14:18:35 +0800

    Update codechallenge12.py

---
+++ codechallenge12.py
@@ -0,0 +1,9 @@
+
+for j in range(1, 7, 1):
+    for a in range(6, j, -1):
+        print(" ", end=" ")
+    for b in range(j, 0, -1):
+        print(c, end=" ")
+    for c in range(2, j + 1, 1):
+        print(c, end=" ")
+    print()
