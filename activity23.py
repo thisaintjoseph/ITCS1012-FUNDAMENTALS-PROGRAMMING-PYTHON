c43285c188c546e41229642c3601687753a13a6f
Joseph <josefuu91@gmail.com>
2025-11-30 12:49:00 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,20 @@
+
+
+#append
+food = ['candy', 'adobro', 'spag', 'choco', 'chicken', 'doughnut']
+
+food.append('rice')
+print(food)
+food.pop()
+print(food)
+food.append('tea')
+print(food)
+
+for j in food:
+    print(f"{j}, anonymous food")
+
+full_name = 'Joshua Garcia'
+# list my name
+another = list(full_name)
+#another reverse
+print(another)
