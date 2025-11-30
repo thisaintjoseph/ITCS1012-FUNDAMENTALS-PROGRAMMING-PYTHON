7709db6c04b530fc9493fe6aad8688ecb8be5c23
Joseph <josefuu91@gmail.com>
2025-11-30 13:08:07 +0800

    Fix variable name and correct discount calculation

--- Activity10.py
+++ Activity10.py
@@ -7,8 +7,8 @@ item = input("What item(Chicken/Pork): ")
 number = eval(input("How many(1-10): "))
 price = eval(input("How much? "))
 PWD = input("are you PWD or Senior(Yes/No): ")
-equal = number * fr * .05
-man = number * fr
+equal = number * price * .05
+man = number * price
 more = man - equal
 
 print("---------------------------")
@@ -17,3 +17,4 @@ if pwd.lower() == "yes":
 	print("Hello", name, "\nItem: ",item, "\nQuantity: ",number,"\nPrice each: ",price,"\nTotal: ",man ,"\nDiscount:",equal,"\nTotal w/discount: ",more)
 else
 	print("Hello", name, "\nItem: ",item, "\nQuantity: ",number,"\nPrice each: ",price,"\nTotal: ",man ,"\nDiscount:","\nTotal w/discount: ",man)
+
