e43b06ed9c0b91255a7a2027bb98aa82af153557
Joseph <josefuu91@gmail.com>
2025-11-30 11:45:40 +0800

    Add files via upload

---
+++ Activity10.py
@@ -0,0 +1,19 @@
+
+
+print("WELCOME to Grocery!!")
+
+name = input("What is your Name? ")
+item = input("What item(Chicken/Pork): ")
+number = eval(input("How many(1-10): "))
+price = eval(input("How much? "))
+PWD = input("are you PWD or Senior(Yes/No): ")
+equal = number * fr * .05
+man = number * fr
+more = man - equal
+
+print("---------------------------")
+
+if pwd.lower() == "yes":
+	print("Hello", name, "\nItem: ",item, "\nQuantity: ",number,"\nPrice each: ",price,"\nTotal: ",man ,"\nDiscount:",equal,"\nTotal w/discount: ",more)
+else
+	print("Hello", name, "\nItem: ",item, "\nQuantity: ",number,"\nPrice each: ",price,"\nTotal: ",man ,"\nDiscount:","\nTotal w/discount: ",man)
