17d917b9bb0b13e87c08ea06ddaa2119c3122db6
Joseph <josefuu91@gmail.com>
2025-11-30 14:20:13 +0800

    Update codechallenge12.py

--- codechallenge12.py
+++ codechallenge12.py
@@ -3,7 +3,8 @@ for j in range(1, 7, 1):
     for a in range(6, j, -1):
         print(" ", end=" ")
     for b in range(j, 0, -1):
-        print(c, end=" ")
+        print(b, end=" ")
     for c in range(2, j + 1, 1):
         print(c, end=" ")
     print()
+
