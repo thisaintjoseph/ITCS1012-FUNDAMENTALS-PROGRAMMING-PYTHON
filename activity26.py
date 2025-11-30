67d1e0559c2267c5b962158094245129ccbec471
Joseph <josefuu91@gmail.com>
2025-11-30 14:04:30 +0800

    Add lists and dictionaries of programming languages

---
+++ activity26.py
@@ -0,0 +1,9 @@
+#dictionary
+
+programming_language = ['Python', 'TurboC', 'C++', 'JavaScript', 'Perl', 'Java']
+
+
+programming_language2 = {'easy':'Python','medyo luma na':'TurboC','mahirap':'C++','mas mahirap':'JavaScript','medyo mahirap':'Perl','napaka hirap':'Java'}
+
+print(programming_language)
+print(programming_language2)
